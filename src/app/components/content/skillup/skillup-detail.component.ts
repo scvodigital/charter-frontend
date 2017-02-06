a4f803393ce474f90840aa7b9e53dd6c80c89ffc
Calum MacUisdean <calum@martinmacuisdean.eu>
2017-02-06 16:23:34 +0000

    Sign up and images updated

--- src/app/components/content/skillup/skillup-detail.component.ts
+++ src/app/components/content/skillup/skillup-detail.component.ts
@@ -10,6 +10,7 @@ import { AppService } from '../../../services/app.service';
 export class SkillUpDetailComponent {
     public number: number;
     public title: string;
+    public illustration: string;
     public content_organisations: string;
     public content_individuals: string;
     public content_safety: string;
@@ -20,6 +21,7 @@ export class SkillUpDetailComponent {
                 if (event.url == '/skill-up/managing-info') {
                     this.number = 1;
                     this.title = 'Managing Info';
+                    this.illustration = 'Managing-Info-RGB-04.png';
                     this.content_organisations = 'Managing Info - organisations content';
                     this.content_individuals = 'Managing Info - individuals content';
                     this.content_safety = 'Managing Info - safety & security content';
@@ -27,6 +29,7 @@ export class SkillUpDetailComponent {
                 } else if (event.url == '/skill-up/communicating') {
                     this.number = 2;
                     this.title = 'Communicating';
+                    this.illustration = 'Communicating-RGB-04.png';
                     this.content_organisations = 'Communicating - organisations content';
                     this.content_individuals = 'Communicating - individuals content';
                     this.content_safety = 'Communicating - safety & security content';
@@ -34,6 +37,7 @@ export class SkillUpDetailComponent {
                 } else if (event.url == '/skill-up/transacting') {
                     this.number = 3;
                     this.title = 'Transacting';
+                    this.illustration = 'Transacting-RGB-04.png';
                     this.content_organisations = 'Transacting - organisations content';
                     this.content_individuals = 'Transacting - individuals content';
                     this.content_safety = 'Transacting - safety & security content';
@@ -41,6 +45,7 @@ export class SkillUpDetailComponent {
                 } else if (event.url == '/skill-up/problem-solving') {
                     this.number = 4;
                     this.title = 'Problem Solving';
+                    this.illustration = 'Problem-Solving-RGB-04.png';
                     this.content_organisations = 'Problem Solving - organisations content';
                     this.content_individuals = 'Problem Solving - individuals content';
                     this.content_safety = 'Problem Solving - safety & security content';
@@ -48,6 +53,7 @@ export class SkillUpDetailComponent {
                 } else if (event.url == '/skill-up/creating') {
                     this.number = 5;
                     this.title = 'Creating';
+                    this.illustration = 'Creating-RGB-04.png';
                     this.content_organisations = 'Creating - organisations content';
                     this.content_individuals = 'Creating - individuals content';
                     this.content_safety = 'Creating - safety & security content';
